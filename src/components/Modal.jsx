a852823e3b15358b6f767ed27945a9b9f1c209e0
erain <enesyagmur99@gmail.com>
2025-05-31 09:07:00 +0300

    modal için yükseklik ayarı yapıldı

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ setModalOpen, question, setAnswer }) => {
   };
 
   return (
-    <div className="w-full h-[590px] fixed inset-0 z-50 flex items-center justify-center rounded-lg bg-black bg-opacity-50">
+    <div className="w-full h-[570px] fixed inset-0 z-50 flex items-center justify-center rounded-lg bg-black bg-opacity-50">
       <div className="bg-white w-11/12  rounded-xl shadow-lg p-20">
         <p className="text-xl font-bold text-center mb-3 text-red-400">
           {question}?
